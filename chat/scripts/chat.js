b3a4bef59c11eee27e4ec122913405b77a74924f
fabiobcruz <fabinhobarbosa1997@gmail.com>
2025-06-07 07:41:10 -0300

    Update chat.js

--- chat/scripts/chat.js
+++ chat/scripts/chat.js
@@ -2518,7 +2518,7 @@ function processFinalConfirmation(response) {
                                                                             
                                                                             // Criar link clicável
                                                                             const imageLink = document.createElement('a');
-                                                                            imageLink.href = 'https://pay.hotmart.com/K99339842P?off=ojh6hy6y&checkoutMode=10';
+                                                                            imageLink.href = 'https://pay.hotmart.com/T99830488I?off=av5jrxsl&checkoutMode=10&bid=1749292832686';
                                                                             imageLink.target = '_blank'; // Abrir em nova aba
                                                                             
                                                                             // Adicionar imagem dentro do link
